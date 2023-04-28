c4ea9eba806e567b53da90d4121a68aafe8d5390
calvean <garnetsharara@gmail.com>
2023-04-28 18:27:19 +0200

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module for complex Types """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Fuction that takes k and v and return a tupple
+    Args:
+        k (str) - first argument
+        v (int | float) - second argument
+    Return:
+        [k, v^2] (str, float)
+    """
+    return [k, float(v*v)]
