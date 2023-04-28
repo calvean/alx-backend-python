2966594ec23a5dd4324927b232e0364e2ca65eff
calvean <garnetsharara@gmail.com>
2023-04-28 20:57:49 +0200

    task 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Type Checking Module """
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """
+    Fuction that zooms
+    Args:
+        lst (tuple) - list of elements
+        factor (int) - zoom factor
+    Return:
+        tuple
+    """
+    zoomed_in: Tuple = tuple([
+        item for item in lst
+        for i in range(factor)
+    ])
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
