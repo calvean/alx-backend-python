4e27146248af6d22cc8879becba32f873980149a
calvean <garnetsharara@gmail.com>
2023-04-28 15:09:00 +0200

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Floor Module calculate floor of a number """
+
+
+def floor(n: float) -> int:
+    """
+    Function that returns the floor of a number
+    Args:
+        n (float) - input number
+    Return:
+        (int) - floor of a number
+    """
+    return int(n)
