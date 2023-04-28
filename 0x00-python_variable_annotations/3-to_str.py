aba381ed21d93d9a1684bfc59c513bd6e92640b7
calvean <garnetsharara@gmail.com>
2023-04-28 15:14:54 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module for float to string """
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns a string representation of a float
+    Args:
+        n (float) - arg to be converted
+    Return:
+        (str) - string representation
+    """
+    return str(n)
