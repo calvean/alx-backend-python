617b8c50360b1e5fbbdf310b027f5275539d2941
calvean <garnetsharara@gmail.com>
2023-04-28 14:49:05 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" This Module Adds two numbers """
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers
+    Args:
+        a (float)
+        b (float)
+    Return:
+        sum (float)
+    """
+    return a + b
