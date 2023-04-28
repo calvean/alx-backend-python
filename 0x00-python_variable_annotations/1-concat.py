f8c63f78d87364c1c5974a87b0442fad3c7d43d1
calvean <garnetsharara@gmail.com>
2023-04-28 14:59:21 +0200

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" This Module Concates two Strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concates two strings
+    Args:
+        str1 (str) - first tring
+        str2 (str) - second string
+    Return:
+        str1 + str2
+    """
+    return str1 + str2
