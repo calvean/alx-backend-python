b457c5659ba5fe449f57b46c64e7998f7bafd065
calvean <garnetsharara@gmail.com>
2023-05-04 17:50:02 +0200

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" async_generator module """
+import asyncio
+import random
+
+
+async def async_generator() -> None:
+    """Async generator that produce a random number
+        for 10 iterations, after every 10s
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
