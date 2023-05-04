d509593b9059956301786026cc7edd98b806874c
calvean <garnetsharara@gmail.com>
2023-05-04 16:06:05 +0200

    task 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Wait Random Module """
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """
+    asynchronous coroutine
+    Args:
+        max_delay (int) - max delay time
+    Return:
+        delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
