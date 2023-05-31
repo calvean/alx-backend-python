eb3b096453f17ea5efec9ffccd4450ee2f19a349
calvean <garnetsharara@gmail.com>
2023-05-31 11:16:09 +0200

    additions

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test case class for the access_nested_map function."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """
+        Test the access_nested_map function.
+
+        Args:
+          nested_map: A nested map/dictionary.
+          path: A sequence of keys representing the path to the value.
+          expected_result: The expected result of accessing the nested map.
+
+        Returns:
+          None, else raises AssertionError, If the returned value
+           is not equal to the expected result.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
