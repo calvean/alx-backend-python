4cf23a6d8ea81ae1b05ac62bc726b84b0b6c4a7a
calvean <garnetsharara@gmail.com>
2023-05-31 11:23:42 +0200

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,3 +25,27 @@ class TestAccessNestedMap(unittest.TestCase):
            is not equal to the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test that a KeyError is raised when accessing a non-existing key.
+
+        Args:
+          nested_map: A nested map/dictionary.
+          path: A sequence of keys representing the path to the value.
+
+        Returns:
+          None, else raise AssertionError: If a KeyError is not raised
+           or the exception message is not as expected.
+        """
+
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        actual_exception_msg = str(context.exception)
+        expected_exception_msg = f"Key not found: {path[-1]}"
+
+        self.assertEqual(actual_exception_msg, expected_exception_msg)
